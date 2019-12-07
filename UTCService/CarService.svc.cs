c7b2af1dd54c9b76ce549c8bba49b1df45f4bbf3
adammarc93 <adammarc93@gmail.com>
2019-12-07 17:36:24 +0100

    Added CRUD operations to service

--- UTCService/CarService.svc.cs
+++ UTCService/CarService.svc.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -8,26 +10,129 @@ using System.Text;
 
 namespace UTCService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class CarService : ICarService
     {
-        public string GetData(int value)
+        private SqlConnection con;
+
+        public CarService()
         {
-            return string.Format("You entered: {0}", value);
+            con = new SqlConnection("Data Source=DESKTOP-BSBRCG2\\LENOVOG50;Initial Catalog=UTC;User ID=sa;Password=FokaKura12!;");
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public List<Car> GetCars()
         {
-            if (composite == null)
+            var ds = new DataSet();
+            var result = new List<Car>();
+
+            try
             {
-                throw new ArgumentNullException("composite");
+                string Query = "SELECT * FROM [UTC].[dbo].Cars";
+
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+                sda.Fill(ds);
+
+                foreach (DataRow record in ds.Tables[0].Rows)
+                {
+                    result.Add(new Car
+                    {
+                        Id = Convert.ToInt32(record["Id"]),
+                        Brand = Convert.ToString(record["Brand"]),
+                        Model = Convert.ToString(record["Model"]),
+                        TotalCost = Convert.ToInt32(record["TotalCost"]),
+                        YearOfProduction = Convert.ToInt32(record["YearOfProduction"]),
+                        Mileage = Convert.ToInt32(record["Mileage"]),
+                        Fuel = Convert.ToString(record["Fuel"]),
+                        CarType = Convert.ToString(record["CarType"]),
+                        Seats = Convert.ToInt32(record["Seats"]),
+                        Color = Convert.ToString(record["Color"]),
+                        CarStatus = Convert.ToString(record["CarStatus"])
+                    });
+                }
+
             }
-            if (composite.BoolValue)
+            catch (FaultException ex)
             {
-                composite.StringValue += "Suffix";
+                throw new FaultException<string>("Error: " + ex);
             }
-            return composite;
+
+            return result;
+        }
+
+        public void PostCar(Car newCar)
+        {
+            try
+            {
+                string Query = @"INSERT INTO [UTC].[dbo].Cars (Brand, Model, TotalCost, YearOfProduction, Mileage, Fuel, CarType, Seats, Color, CarStatus)
+	                             VALUES (@Brand, @Model, @TotalCost, @YearOfProduction, @Mileage, @Fuel, @CarType, @Seats, @Color, @CarStatus)";
+                var cmd = new SqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@Brand", newCar.Brand);
+                cmd.Parameters.AddWithValue("@Model", newCar.Model);
+                cmd.Parameters.AddWithValue("@TotalCost", newCar.TotalCost);
+                cmd.Parameters.AddWithValue("@YearOfProduction", newCar.YearOfProduction);
+                cmd.Parameters.AddWithValue("@Mileage", newCar.Mileage);
+                cmd.Parameters.AddWithValue("@Fuel", newCar.Fuel);
+                cmd.Parameters.AddWithValue("@CarType", newCar.CarType);
+                cmd.Parameters.AddWithValue("@Seats", newCar.Seats);
+                cmd.Parameters.AddWithValue("@Color", newCar.Color);
+                cmd.Parameters.AddWithValue("@CarStatus", newCar.CarStatus);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (FaultException ex)
+            {
+                throw new FaultException<string>("Error: " + ex);
+            }
+        }
+
+        public void PutCar(Car newCar)
+        {
+            try
+            {
+                string Query = "UPDATE [UTC].[dbo].Cars "
+                                + "SET Brand=@Brand, Model=@Model, TotalCost=@TotalCost, YearOfProduction=@YearOfProduction, "
+                                + "Mileage=@Mileage, Fuel=@Fuel, CarType=@CarType, Color=@Color, CarStatus=@CarStatus "
+                                + "WHERE Id=@Id";
+                var cmd = new SqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@Id", newCar.Id);
+                cmd.Parameters.AddWithValue("@Brand", newCar.Brand);
+                cmd.Parameters.AddWithValue("@Model", newCar.Model);
+                cmd.Parameters.AddWithValue("@TotalCost", newCar.TotalCost);
+                cmd.Parameters.AddWithValue("@YearOfProduction", newCar.YearOfProduction);
+                cmd.Parameters.AddWithValue("@Mileage", newCar.Mileage);
+                cmd.Parameters.AddWithValue("@Fuel", newCar.Fuel);
+                cmd.Parameters.AddWithValue("@CarType", newCar.CarType);
+                cmd.Parameters.AddWithValue("@Seats", newCar.Seats);
+                cmd.Parameters.AddWithValue("@Color", newCar.Color);
+                cmd.Parameters.AddWithValue("@CarStatus", newCar.CarStatus);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<string>("Error: " + ex);
+            }
+        }
+
+        public void DeleteCar(int id)
+        {
+            try
+            {
+                string Query = "DELETE FROM [UTC].[dbo].Cars WHERE Id=@Id";
+                var cmd = new SqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException<string>("Error: " + ex);
+            }
         }
     }
 }
--- UTCService/ICarService.cs
+++ UTCService/ICarService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -8,40 +9,112 @@ using System.Text;
 
 namespace UTCService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface ICarService
     {
+        [OperationContract]
+        List<Car> GetCars();
 
         [OperationContract]
-        string GetData(int value);
+        void PostCar(Car newCar);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        void PutCar(Car newCar);
 
-        // TODO: Add your service operations here
+        [OperationContract]
+        void DeleteCar(int id);
     }
 
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
-    public class CompositeType
+    public class Car
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+        private int id;
+        private string brand;
+        private string model;
+        private int totalCost;
+        private int yearOfProduction;
+        private int mileage;
+        private string fuel;
+        private string carType;
+        private int seats;
+        private string color;
+        private string carStatus;
 
         [DataMember]
-        public bool BoolValue
+        public int Id
         {
-            get { return boolValue; }
-            set { boolValue = value; }
+            get { return id; }
+            set { id = value; }
         }
 
         [DataMember]
-        public string StringValue
+        public string Brand
         {
-            get { return stringValue; }
-            set { stringValue = value; }
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        [DataMember]
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        [DataMember]
+        public int TotalCost
+        {
+            get { return totalCost; }
+            set { totalCost = value; }
+        }
+
+        [DataMember]
+        public int YearOfProduction
+        {
+            get { return yearOfProduction; }
+            set { yearOfProduction = value; }
+        }
+
+        [DataMember]
+        public int Mileage
+        {
+            get { return mileage; }
+            set { mileage = value; }
+        }
+
+        [DataMember]
+        public string Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        [DataMember]
+        public string CarType
+        {
+            get { return carType; }
+            set { carType = value; }
+        }
+
+        [DataMember]
+        public int Seats
+        {
+            get { return seats; }
+            set { seats = value; }
+        }
+
+        [DataMember]
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        [DataMember]
+        public string CarStatus
+        {
+            get { return carStatus; }
+            set { carStatus = value; }
         }
     }
 }
