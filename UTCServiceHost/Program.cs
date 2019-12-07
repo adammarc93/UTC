efbc5e9991907920f81e530c06f5934a2219dec7
adammarc93 <adammarc93@gmail.com>
2019-12-07 17:52:37 +0100

    Added Console App starting service

---
+++ UTCServiceHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using UTCService;
+
+namespace UTCServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/UTC/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(CarService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICarService), new WSHttpBinding(), "CarService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
